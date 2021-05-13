8ebc32939bbf8710052663774c7bce1d65fc7561
fdietrich <dietrifa@rhrk.uni-kl.de>
2021-05-13 11:47:39 +0200

    ListMap habe ich, wie im Video gezeigt hinzugefügt.

---
+++ MP1/src/main/java/de/tukl/programmierpraktikum2021/mp1/ListMap.java
@@ -0,0 +1,28 @@
+package de.tukl.programmierpraktikum2021.mp1;
+
+public class ListMap<K, V> implements Map<K, V>{
+    @Override
+    public V get(K key) {
+        return null;
+    }
+
+    @Override
+    public void put(K key, V value) {
+
+    }
+
+    @Override
+    public void remove(K key) {
+
+    }
+
+    @Override
+    public int size() {
+        return 0;
+    }
+
+    @Override
+    public void keys(K[] array) {
+
+    }
+}
