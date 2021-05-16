779fd6ee8308318d4b837c894fa41c19160689ce
hjebali <houssem.ed.jebali@gmail.com>
2021-05-16 18:32:43 +0200

    missing sorting

--- MP1/src/main/java/de/tukl/programmierpraktikum2021/mp1/BibleAnalyzer.java
+++ MP1/src/main/java/de/tukl/programmierpraktikum2021/mp1/BibleAnalyzer.java
@@ -2,11 +2,27 @@ package de.tukl.programmierpraktikum2021.mp1;
 
 public class BibleAnalyzer {
     public static void countWords(Map<String, Integer> counts) {
-        throw new RuntimeException("TODO");
+        int counter = 0;
+        for (String fixed : Util.getBibleWords()) {
+            counter = 0;
+            if(counts.get(fixed) == null){
+                for (String word : Util.getBibleWords()) {
+                    if(word.equals(fixed)) counter++;
+                }
+                counts.put(fixed, counter);
+            }
+        }
     }
 
     public static void main(String[] args) {
-        throw new RuntimeException("TODO");
+        Map<String, Integer> map = new ListMap<>();
+        String[] words = new String[map.size()];
+        map.keys(words);
+        // Sort words TODO
+        for (String word : words) {
+            System.out.println(map.get(word) + " " + word + "\n");
+        }
+
     }
 
     public static void sort(String[] words, Map<String, Integer> counts) {
