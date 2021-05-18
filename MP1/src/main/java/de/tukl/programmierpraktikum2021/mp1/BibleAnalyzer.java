21dc96eb1057d854e2827b224f4801d1a25a8696
hjebali <houssem.ed.jebali@gmail.com>
2021-05-18 20:10:26 +0200

    aufgabe 4 done

--- MP1/src/main/java/de/tukl/programmierpraktikum2021/mp1/BibleAnalyzer.java
+++ MP1/src/main/java/de/tukl/programmierpraktikum2021/mp1/BibleAnalyzer.java
@@ -15,13 +15,61 @@ public class BibleAnalyzer {
         countWords(map);
         String[] words = new String[map.size()];
         map.keys(words);
-        // Sort words TODO
+        sort(words, map);
         for (String word : words) {
             System.out.println(map.get(word) + " " + word);
         }
     }
 
+    /**
+     * Sorts an array using Heap Sort
+     **/
     public static void sort(String[] words, Map<String, Integer> counts) {
-        throw new RuntimeException("TODO");
+        int n = words.length - 1;
+
+        // Create a Heap
+        for (int i = n / 2; i >= 0; i--)
+            heapify(words, counts, n, i);
+
+        // Sort the largest element then heapify
+        for (int i = n ; i > 0; i--) {
+            swap(words, i, 0);
+            heapify(words, counts, i, 0);
+        }
+    }
+
+    /**
+     * Transform a given array into a heap
+     *
+     * @param arr   the array to be heapified.
+     * @param map   used as reference to compare elements in arr.
+     * @param n     the last index of arr.
+     * @param root  the current root of the heap.
+     */
+    private static void heapify(String[] arr, Map<String, Integer> map, int n, int root) {
+        int tmpRoot = root; // used to determine which node to swap to
+        int l = 2 * root + 1; // Left node
+        int r = 2 * root + 2; // Right node
+
+        // Check left node and set it as tmp root if its larger
+        if (l < n && map.get(arr[l]) > map.get(arr[tmpRoot]))
+            tmpRoot = l;
+
+        // Check right node and set it as tmp root if its larger
+        if (r < n && map.get(arr[r]) > map.get(arr[tmpRoot]))
+            tmpRoot = r;
+
+        // If one of the node is larger than root swap and heapify sub trees
+        if (tmpRoot != root) {
+            swap(arr, root, tmpRoot);
+            heapify(arr, map, n, tmpRoot);
+        }
+    }
+
+    // Swap two elements in an array
+    private static void swap(String[] arr, int a, int b) {
+        String tmp = arr[a];
+        arr[a] = arr[b];
+        arr[b] = tmp;
     }
 }
