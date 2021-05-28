8d18e7a80d77d04e7c31c991fd6e607a048aa545
Nhat Anh Pham <npham@rhrk.uni-kl.de>
2021-05-28 23:57:41 +0200

    TrieMap implementiert.

--- MP2/src/main/java/de/tukl/programmierpraktikum2021/mp2/TrieMap.java
+++ MP2/src/main/java/de/tukl/programmierpraktikum2021/mp2/TrieMap.java
@@ -1,31 +1,44 @@
 package de.tukl.programmierpraktikum2021.mp2;
 import com.googlecode.concurrenttrees.common.KeyValuePair;
+import com.googlecode.concurrenttrees.common.PrettyPrinter;
+import com.googlecode.concurrenttrees.radix.ConcurrentRadixTree;
+import com.googlecode.concurrenttrees.radix.node.concrete.DefaultCharArrayNodeFactory;
 
 public class TrieMap<V> implements Trie<V> {
-    private com.googlecode.concurrenttrees.radix.ConcurrentRadixTree<V> map;
+    private final com.googlecode.concurrenttrees.radix.ConcurrentRadixTree<V> map;
+
+    public TrieMap() {
+        this.map = new ConcurrentRadixTree<>(new DefaultCharArrayNodeFactory());
+    }
 
     @Override
     public V get(String key) {
-        throw new RuntimeException("TODO");
+        return map.getValueForExactKey(key);
     }
 
     @Override
     public void put(String key, V value) {
-        throw new RuntimeException("TODO");
+        map.put(key, value);
     }
 
     @Override
     public void remove(String key) {
-        throw new RuntimeException("TODO");
+        map.remove(key);
     }
 
     @Override
     public int size() {
-        throw new RuntimeException("TODO");
+        return map.size();
     }
 
     @Override
     public Iterable<KeyValuePair<V>> searchKeyPrefix(String prefix) {
-        throw new RuntimeException("TODO");
+        return map.getKeyValuePairsForKeysStartingWith(prefix);
+    }
+
+    // For testing
+    @Override
+    public String toString() {
+        return PrettyPrinter.prettyPrint(map);
     }
 }
