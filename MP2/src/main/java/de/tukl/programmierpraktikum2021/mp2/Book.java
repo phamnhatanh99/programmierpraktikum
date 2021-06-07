4a21f2367f456ea5287d46753ae635831451d414
Fabian Dietrich <dietrifa@rhrk.uni-kl.de>
2021-06-07 07:15:44 +0000

    Book.java

--- MP2/src/main/java/de/tukl/programmierpraktikum2021/mp2/Book.java
+++ MP2/src/main/java/de/tukl/programmierpraktikum2021/mp2/Book.java
@@ -10,16 +10,4 @@ public class Book {
         this.author = author;
         this.year = year;
     }
-
-    public String getTitle() {
-        return title;
-    }
-
-    public String getAuthor() {
-        return author;
-    }
-
-    public int getYear() {
-        return year;
-    }
 }
