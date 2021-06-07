6e0d314dee724a00ad2fefec078e27192662475f
Fabian Dietrich <dietrifa@rhrk.uni-kl.de>
2021-06-07 07:17:16 +0000

    Main.java implementiert

--- MP2/src/main/java/de/tukl/programmierpraktikum2021/mp2/Main.java
+++ MP2/src/main/java/de/tukl/programmierpraktikum2021/mp2/Main.java
@@ -1,6 +1,6 @@
 package de.tukl.programmierpraktikum2021.mp2;
-import java.util.*; // Noch vor der Klassendefinition importieren
-//Quelle: https://www.inf-schule.de/programmierung/oopjava/anhang/java 26.05.2021 12:13 Uhr
+import java.util.*;
+//Quelle zum Einlesen von Benutzereingaben: https://www.inf-schule.de/programmierung/oopjava/anhang/java 26.05.2021 12:13 Uhr
 
 public class Main {
     public static void main(String[] args) {
@@ -8,16 +8,16 @@ public class Main {
         for (Book book: Util.books){
             kwicimpl.add(book);
         }
-        System.out.println("Please enter a search term! \n search term:");
+        System.out.println("Please enter a search term! \n Search term:");
         Scanner scanner = new Scanner(System.in);
         String input = scanner.nextLine();
         List<Book> matches = kwicimpl.search(input);
-        if (matches == null){
+        if (matches.isEmpty()){
             System.out.println("Sorry, no match found for "+ input + ".");
         }
         else{
             for (Book match :matches) {
-                System.out.println("Author: " + match.getAuthor() + "\nTitle: " + match.getTitle() + "\nYear: " + match.getYear());
+                System.out.println("Author: " + match.author + "\nTitle: " + match.title + "\nYear: " + match.year);
             }
         }
     }
