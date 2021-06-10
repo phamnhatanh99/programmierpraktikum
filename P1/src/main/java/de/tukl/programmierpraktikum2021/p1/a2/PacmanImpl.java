dc5eac1fe60208357272a58e60601e15f4ca446c
fdietrich <dietrifa@rhrk.uni-kl.de>
2021-06-10 14:09:10 +0200

    Aufgabe 2a soweit ich das ohne Tests beurteilen kann müsste die Funktionieren

---
+++ P1/src/main/java/de/tukl/programmierpraktikum2021/p1/a2/PacmanImpl.java
@@ -0,0 +1,60 @@
+package de.tukl.programmierpraktikum2021.p1.a2;
+
+import de.tukl.programmierpraktikum2021.p1.a1.DuplicateEdgeException;
+import de.tukl.programmierpraktikum2021.p1.a1.GraphImpl;
+import de.tukl.programmierpraktikum2021.p1.a1.InvalidNodeException;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.Set;
+
+public class PacmanImpl implements Pacman {
+
+    @Override
+    public void buildDependencyGraph() throws IOException {
+        Util u = new Util();
+        GraphImpl g = new GraphImpl();
+        for (String normalPackage : u.getAllNormalPackages()) {
+            g.addNode(normalPackage, u.getVersion(normalPackage));
+        }
+        for (String virtualPackage : u.getAllVirtualPackages()) {
+            g.addNode(virtualPackage, u.getVersion(virtualPackage));
+        }
+        for (String node : g.getNodeIds()) {
+            for (String dependence : u.getDependencies(node)) {
+                try {
+                    g.addEdge(node, dependence);
+                } catch (Exception e) {
+                    break;
+                }
+            }
+            for (String virtualPackage : u.getVirtual(node)) {
+                try {
+                    g.addEdge(node, virtualPackage);
+                } catch (Exception e) {
+                    break;
+                }
+            }
+        }
+    }
+
+    @Override
+    public Set<String> whoRequires(String pkg) throws InvalidNodeException {
+        return null;
+    }
+
+    @Override
+    public String transitiveDependencies(String pkg) throws InvalidNodeException {
+        return null;
+    }
+
+    @Override
+    public List<Package> buildInstallList(String pkg) throws InvalidNodeException {
+        return null;
+    }
+
+    @Override
+    public void install(String pkg) throws InvalidNodeException {
+
+    }
+}
